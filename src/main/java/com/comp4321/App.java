69f41b046d41d8e7e48d6dcef7334bdd6f6ff353
sungjin122517 <sungjin122517@naver.com>
2024-03-24 22:08:31 +0800

    separate crawl and output file based on main arg

--- src/main/java/com/comp4321/App.java
+++ src/main/java/com/comp4321/App.java
@@ -7,18 +7,30 @@ import org.htmlparser.util.ParserException;
 import com.comp4321.indexers.Indexer;
 
 public class App {
-    public static void main(String[] arg) {
+    public static void main(String[] args) {
         final var baseURL = "https://www.cse.ust.hk/~kwtleung/COMP4321/testpage.htm";
         final var maxPages = 30;
 
-        try (final var indexer = new Indexer()) {
-            final var crawler = new Crawler(baseURL);
-            crawler.bfs(maxPages, indexer::indexDocument);
-            indexer.printAll();
-            indexer.outputSpiderResult();
-        } catch (ParserException | IOException e) {
-            e.printStackTrace();
-        }
-        
+        if (args.length > 0) {
+            try (final var indexer = new Indexer()) {
+                final var crawler = new Crawler(baseURL);
+
+                switch (args[0]) {
+                    case "crawl":
+                        crawler.bfs(maxPages, indexer::indexDocument);
+                        break;
+                    case "spider_result":
+                        indexer.outputSpiderResult("spider_result.txt");
+                        break;
+                    default:
+                        System.out.println("Unknown argument. Please use 'crawl' or 'spider_result'");
+                        break;
+                }
+            } catch (ParserException | IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Please provide an argument. Use 'crawl' or 'spider_result'");
+        }        
     }
 }
